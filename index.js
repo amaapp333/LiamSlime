ee3b8a8219a7b1896db32bd2795c350c56aab66b
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-12 23:38:10 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+export Dimensions from './constants/Dimensions';
+export Format from './constants/Format';
+export Ad from './GooglePublisherTag';
+export Mobile from './Mobile';
+export Horizontal from './Horizontal';
+
+export default from './GooglePublisherTag';
